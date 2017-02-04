712a072a3fb3c7164026deb599dcadef4ba54ebf
Andreas Pardeike <andreas@pardeike.net>
2017-02-04 12:01:57 +0100

    fixing .gitignore

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,58 +1,45 @@
 ﻿using Verse;
 using Harmony;
-using Verse.AI;
 using RimWorld;
 using System.Linq;
-using System.IO;
-using System.Reflection;
+using Harmony.ILCopying;
+using System;
 
 namespace SameSpot
 {
 	[StaticConstructorOnStartup]
-	[HarmonyPatch]
 	class Main
 	{
 		static Main()
 		{
-			var path = GetHarmonyConfigFile();
-			var harmony = HarmonyInstance.RegisterWithFile(path);
-			harmony.PatchAll(typeof(Main).Module);
-		}
+			var harmony = HarmonyInstance.Create("net.pardeike.rimworld.mod.camera+");
+			var processor = new HarmonyProcessor(Priority.Normal, new string[0], new string[0]);
+			processor.AddILProcessor(new MethodReplacer(
+				AccessTools.Method(typeof(PawnDestinationManager), "DestinationIsReserved", new Type[] { typeof(IntVec3), typeof(Pawn) }),
+				AccessTools.Method(typeof(BestOrderedGotoDestNearPatcher), "DestinationIsReserved")
+			));
+			processor.AddILProcessor(new MethodReplacer(
+				AccessTools.Method(typeof(GenGrid), "Standable"),
+				AccessTools.Method(typeof(BestOrderedGotoDestNearPatcher), "Standable")
+			));
 
-		static string GetHarmonyConfigFile()
-		{
-			var myModule = typeof(Main).Module;
-			ModContentPack modInfo = LoadedModManager.RunningMods
-				.FirstOrDefault(mcp => mcp.assemblies.loadedAssemblies
-					.ToList().SelectMany(a => a.GetLoadedModules()).Contains(myModule));
-			if (modInfo == null) throw new InvalidDataException("Cannot find mod for module " + myModule);
-			var sep = Path.DirectorySeparatorChar;
-			return string.Concat(new object[] { modInfo.RootDir, sep, "About", sep, "Harmony.txt" });
-		}
-
-		static MethodInfo TargetMethod()
-		{
 			var predicateClass = typeof(RCellFinder).GetNestedTypes(AccessTools.all)
 				.FirstOrDefault(t => t.FullName.Contains("BestOrderedGotoDestNear"));
-			if (predicateClass == null) return null;
-			var method = predicateClass.GetMethods(AccessTools.all).FirstOrDefault(m => m.ReturnType == typeof(bool));
-			if (method == null)
-				Log.Warning("Error: Cannot find and patch BestOrderedGotoDestNear.Predicate. SameSpot mod won't work, sorry.");
-			return method;
+			var original = predicateClass.GetMethods(AccessTools.all).FirstOrDefault(m => m.ReturnType == typeof(bool));
+			harmony.Patch(original, null, null, processor);
+		}
+	}
+
+	public static class BestOrderedGotoDestNearPatcher
+	{
+		public static bool DestinationIsReserved(this PawnDestinationManager instance, IntVec3 c, Pawn searcher)
+		{
+			return false;
 		}
 
-		static void Postfix(object instance, ref bool result, ref IntVec3 cell)
+		public static bool Standable(this IntVec3 c, Map map)
 		{
-			if (result == false)
-			{
-				var selector = Find.Selector;
-				var isSingleColonistSelected = (selector.SelectedObjects.Count == 1 && selector.SelectedObjects[0] is Pawn);
-				if (isSingleColonistSelected)
-				{
-					var searcher = Traverse.Create(instance).Field("searcher").GetValue<Pawn>();
-					result = searcher.CanReach(cell, PathEndMode.OnCell, Danger.Deadly, false, TraverseMode.ByPawn);
-				}
-			}
+			return true;
 		}
 	}
 }
