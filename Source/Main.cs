4e282ff411830034c2a2949e257023f29ab9e57c
Andreas Pardeike <andreas@pardeike.net>
2017-10-27 21:23:02 +0200

    Update for A18

--- Source/Main.cs
+++ Source/Main.cs
@@ -10,30 +10,37 @@ using System.Collections.Generic;
 namespace SameSpot
 {
 	[StaticConstructorOnStartup]
-	class Main
+	static class SameSpotMod
 	{
-		static Main()
+		static SameSpotMod()
 		{
 			var harmony = HarmonyInstance.Create("net.pardeike.rimworld.mod.samespot");
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 		}
-	}
 
-	[HarmonyPatch]
-	static class BestOrderedGotoDestNearPatcher
-	{
-		static bool DestinationIsReserved(this PawnDestinationManager instance, IntVec3 c, Pawn searcher)
+		public static bool IsReserved(this PawnDestinationReservationManager instance, IntVec3 loc)
 		{
 			if (Find.Selector.SelectedObjects.Count() == 1) return false;
 			if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) return false;
-			return instance.DestinationIsReserved(c, searcher);
+			return instance.IsReserved(loc);
 		}
 
-		static bool Standable(this IntVec3 c, Map map)
+		public static bool CanReserve(this PawnDestinationReservationManager instance, IntVec3 c, Pawn searcher)
+		{
+			if (Find.Selector.SelectedObjects.Count() == 1) return true;
+			if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) return true;
+			return instance.CanReserve(c, searcher);
+		}
+
+		public static bool Standable(this IntVec3 c, Map map)
 		{
 			return true;
 		}
+	}
 
+	[HarmonyPatch]
+	static class RCellFinder_BestOrderedGotoDestNear_Patch
+	{
 		static MethodBase TargetMethod()
 		{
 			var predicateClass = typeof(RCellFinder).GetNestedTypes(AccessTools.all)
@@ -46,13 +53,33 @@ namespace SameSpot
 			return instructions
 
 				.MethodReplacer(
-					AccessTools.Method(typeof(PawnDestinationManager), "DestinationIsReserved", new Type[] { typeof(IntVec3), typeof(Pawn) }),
-					AccessTools.Method(typeof(BestOrderedGotoDestNearPatcher), "DestinationIsReserved")
+					AccessTools.Method(typeof(PawnDestinationReservationManager), nameof(PawnDestinationReservationManager.CanReserve), new Type[] { typeof(IntVec3), typeof(Pawn) }),
+					AccessTools.Method(typeof(SameSpotMod), nameof(SameSpotMod.CanReserve))
 				)
 
 				.MethodReplacer(
-					AccessTools.Method(typeof(GenGrid), "Standable"),
-					AccessTools.Method(typeof(BestOrderedGotoDestNearPatcher), "Standable")
+					AccessTools.Method(typeof(GenGrid), nameof(GenGrid.Standable)),
+					AccessTools.Method(typeof(SameSpotMod), nameof(SameSpotMod.Standable))
+				);
+		}
+	}
+
+	[HarmonyPatch(typeof(JoyGiver_InteractBuildingInteractionCell))]
+	[HarmonyPatch("TryGivePlayJob")]
+	static class JoyGiver_InteractBuildingInteractionCell_TryGivePlayJob_Patch
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			return instructions
+
+				.MethodReplacer(
+					AccessTools.Method(typeof(PawnDestinationReservationManager), nameof(PawnDestinationReservationManager.IsReserved), new Type[] { typeof(IntVec3) }),
+					AccessTools.Method(typeof(SameSpotMod), nameof(SameSpotMod.IsReserved))
+				)
+
+				.MethodReplacer(
+					AccessTools.Method(typeof(GenGrid), nameof(GenGrid.Standable)),
+					AccessTools.Method(typeof(SameSpotMod), nameof(SameSpotMod.Standable))
 				);
 		}
 	}
